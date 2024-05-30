85dfc2b20db39c21386273067d50aef2c7b1053e
Malena Romano <malenalromano2@gmail.com>
2024-05-30 16:49:52 -0300

    apartado e completo

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -3,5 +3,16 @@
 
 int main(){
     printf("hola mundo");
+    int num = 1; //declaracion de variable y puntero
+    int *p_num;
 
+    p_num = &num; //se guarda la direccion de la variable en el puntero
+
+    printf("El contenido del puntero: %d", *p_num);
+    printf("\nLa direccion de memoria almacenada por el puntero: %d", p_num);
+    printf("\nLa direccion de memoria de la variable: %d", &num);
+    printf("\nLa direccion de memoria del puntero: %d", &p_num);
+    printf("\nEl tamaño de memoria utilizado por esa variable: %d bytes", sizeof(num));
+
+    return 0;
 }
